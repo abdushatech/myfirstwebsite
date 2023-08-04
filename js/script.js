5740a2dbf19cfa2116d66d68d0365a9fb92e030b
abdushatech <140687250+abdushatech@users.noreply.github.com>
2023-08-04 19:39:37 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,22 @@ const mobileNavs = document.getElementsByClassName("nav-links")[0]
 hamburger.addEventListener("click", () => {
   mobileNavs.classList.toggle("active")
 })
+
+<!... read more js-->
+  <script>
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+</script>
