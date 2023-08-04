7a4c422ef8767446bc33a6611048bc6470a6d9f4
abdushatech <140687250+abdushatech@users.noreply.github.com>
2023-08-04 19:58:07 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,21 +5,3 @@ hamburger.addEventListener("click", () => {
   mobileNavs.classList.toggle("active")
 })
 
-<!-- read more js-->
-  <script>
-function myFunction() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
-
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more"; 
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less"; 
-    moreText.style.display = "inline";
-  }
-}
-</script>
