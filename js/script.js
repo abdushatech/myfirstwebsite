6df5dc3d7852e5847466de95cf973283b297f0b3
abdushatech <140687250+abdushatech@users.noreply.github.com>
2024-03-04 22:28:57 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburgerButton = document.querySelector('.hamburger');
+  const navLinks = document.querySelector('.nav-links');
+
+  hamburgerButton.addEventListener('click', function () {
+    console.log('Hamburger clicked!');
+    navLinks.classList.toggle('show');
+  });
+});
+
+
+
 const hamburger = document.getElementsByClassName("hamburger")[0]
 const mobileNavs = document.getElementsByClassName("nav-links")[0]
 
